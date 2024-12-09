da6beb2a428679e45999e098f2cc7aba4cb5b3f8
JorimJoram <wkdgyfla97@gmail.com>
2024-12-09 14:24:05 +0900

    test with jupyter lab

---
+++ HyoRim/test.py
@@ -0,0 +1,10 @@
+def hello_world():
+    print("Hello world!")
+def goodbye_world():
+    print("goodbye")
+def goodEvening_world():
+    print("Good Evening")
+
+hello_world()
+goodbye_world()
+goodEvening_world()
